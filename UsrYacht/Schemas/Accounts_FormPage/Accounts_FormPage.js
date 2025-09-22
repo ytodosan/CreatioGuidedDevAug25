e26eeeca978a4b6818dcbc620ea6fe9cc84b64f0
ytodosan <y.todosan@creatio.com>
2025-09-22 21:05:24 +0300

    Account update

---
+++ UsrYacht/Schemas/Accounts_FormPage/Accounts_FormPage.js
@@ -0,0 +1,218 @@
+define("Accounts_FormPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "remove",
+				"name": "AnnualRevenue"
+			},
+			{
+				"operation": "remove",
+				"name": "AddressSearchFilter",
+				"properties": [
+					"targetAttributes"
+				]
+			},
+			{
+				"operation": "merge",
+				"name": "AddressSearchFilter",
+				"values": {
+					"_filterOptions": {
+						"expose": [
+							{
+								"attribute": "AddressSearchFilter_AddressList",
+								"converters": [
+									{
+										"converter": "crt.SearchFilterAttributeConverter",
+										"args": [
+											"AddressList"
+										]
+									}
+								]
+							}
+						],
+						"from": [
+							"AddressSearchFilter_SearchValue",
+							"AddressSearchFilter_FilteredColumnsGroups"
+						]
+					}
+				}
+			},
+			{
+				"operation": "remove",
+				"name": "ContactsSearchFilter",
+				"properties": [
+					"targetAttributes"
+				]
+			},
+			{
+				"operation": "merge",
+				"name": "ContactsSearchFilter",
+				"values": {
+					"_filterOptions": {
+						"expose": [
+							{
+								"attribute": "ContactsSearchFilter_ContactsList",
+								"converters": [
+									{
+										"converter": "crt.SearchFilterAttributeConverter",
+										"args": [
+											"ContactsList"
+										]
+									}
+								]
+							}
+						],
+						"from": [
+							"ContactsSearchFilter_SearchValue",
+							"ContactsSearchFilter_FilteredColumnsGroups"
+						]
+					}
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "Timeline",
+				"values": {
+					"filterValues": "$Timeline_AllTileFilters"
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "TimelineTile_Email",
+				"values": {
+					"filters": "$TimelineTile_Email_Items"
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "TimelineTile_Task",
+				"values": {
+					"filters": "$TimelineTile_Task_Items"
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "TimelineTile_Call",
+				"values": {
+					"filters": "$TimelineTile_Call_Items"
+				}
+			},
+			{
+				"operation": "insert",
+				"name": "Encrypted_CardNumber",
+				"values": {
+					"type": "crt.EncryptedInput",
+					"label": "$Resources.Strings.PDS_UsrCardNumber_afi30tm",
+					"labelPosition": "auto",
+					"control": "$PDS_UsrCardNumber_afi30tm",
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 2,
+						"rowSpan": 1
+					},
+					"visible": true,
+					"readonly": false,
+					"placeholder": "",
+					"tooltip": ""
+				},
+				"parentName": "AccountInfoFieldsContainer",
+				"propertyName": "items",
+				"index": 2
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfigDiff: /**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [
+					"attributes"
+				],
+				"values": {
+					"PDS_UsrCardNumber_afi30tm": {
+						"modelConfig": {
+							"path": "PDS.UsrCardNumber"
+						}
+					},
+					"Timeline_AllTileFilters": {
+						"from": [],
+						"converter": "crt.ToTileFilterGroup"
+					}
+				}
+			},
+			{
+				"operation": "merge",
+				"path": [
+					"attributes",
+					"AddressList",
+					"modelConfig"
+				],
+				"values": {
+					"filterAttributes": [
+						{
+							"name": "AddressList_PredefinedFilter",
+							"loadOnChange": true
+						},
+						{
+							"name": "AddressSearchFilter_AddressList",
+							"loadOnChange": true
+						}
+					]
+				}
+			},
+			{
+				"operation": "merge",
+				"path": [
+					"attributes",
+					"ContactsList",
+					"modelConfig"
+				],
+				"values": {
+					"filterAttributes": [
+						{
+							"name": "ContactsList_PredefinedFilter",
+							"loadOnChange": true
+						},
+						{
+							"name": "ContactsSearchFilter_ContactsList",
+							"loadOnChange": true
+						}
+					]
+				}
+			}
+		]/**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/,
+		modelConfigDiff: /**SCHEMA_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [
+					"dataSources"
+				],
+				"values": {
+					"TimelineTile_EmailDS": {
+						"type": "crt.EntityDataSource",
+						"scope": "viewElement",
+						"config": {
+							"entitySchemaName": "Activity"
+						}
+					},
+					"TimelineTile_TaskDS": {
+						"type": "crt.EntityDataSource",
+						"scope": "viewElement",
+						"config": {
+							"entitySchemaName": "Activity"
+						}
+					},
+					"TimelineTile_CallDS": {
+						"type": "crt.EntityDataSource",
+						"scope": "viewElement",
+						"config": {
+							"entitySchemaName": "Call"
+						}
+					}
+				}
+			}
+		]/**SCHEMA_MODEL_CONFIG_DIFF*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
